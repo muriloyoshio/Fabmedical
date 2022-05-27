39bc6e133ec1579c209092ab07c34f7f869561a8
muriloyoshio <murilo.y.nunes@gmail.com>
2022-05-27 18:18:29 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("f7dc7ae0-e71e-4b0d-8a64-9f32fd9b4fda");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
